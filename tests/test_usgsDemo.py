c2680dcdb427cdc065fa088a7196c55331ff2092
Albert Bode <abode@illinois.edu>
2024-06-06 11:30:44 -0500

    Updated docstrings and commented out large data tests

--- tests/test_usgsDemo.py
+++ tests/test_usgsDemo.py
@@ -6,6 +6,11 @@ from tests.utilities import init_test_log
 
 class Test_usgsDemo:
     def run_test_usgsDemo(self, args, expected_csv_path, log):
+        """
+        Runs usgsDemo with the given arguments.
+        Tests that the csv of results produced by usgsDemo match the expected results.
+        This function is not a test itself.
+        """
         log.info(f'Running usgsDemo with args: {args}')
         os.makedirs('tests/tmp', exist_ok=True)
         test_csv_path = 'tests/tmp/usgsDemo_results.csv'
@@ -27,6 +32,10 @@ class Test_usgsDemo:
         os.remove(test_csv_path)
 
     def test_mock_map(self):
+        """
+        Tests that usgsDemo produces the expected results.
+        Data used for this test is a mock map that is 100x100 pixels. The mock map has two point features, 2_pt and 3_pt and two poly features L_poly, TZ_poly.
+        """
         log = init_test_log('Test_usgsDemo/test_mock_map')
         # args = '-p tests/data/pred_segmentations -t tests/data/true_segmentations -m tests/data/map_images -l tests/data/legends --min_valid_range 5'
         pred_seg_dir = 'tests/data/pred_segmentations'
@@ -44,38 +53,48 @@ class Test_usgsDemo:
         self.run_test_usgsDemo(args, expected_csv_path, log)
         log.info('Test passed successfully')
 
-    def test_MT_OldBaldy(self):
-        log = init_test_log('Test_usgsDemo/test_MT_OldBaldy')
-        # args = '-p tests/uncommited_data/pred_segmentations/MT_OldBaldyMountain_265833_1989_24000_geo_mosaic* -t tests/uncommited_data/true_segmentations -m tests/uncommited_data/map_images/ -l tests/uncommited_data/legends'
-        pred_seg_dir = 'tests/uncommited_data/pred_segmentations'
-        args = SimpleNamespace(
-            pred_segmentations=[os.path.join(pred_seg_dir,f) for f in os.listdir(pred_seg_dir) if 'MT_OldBaldyMountain' in f], 
-            true_segmentations='tests/uncommited_data/true_segmentations', 
-            map_images='tests/uncommited_data/map_images',
-            legends='tests/uncommited_data/legends',
-            min_valid_range=0.1,
-            difficult_weight=0.7,
-            set_false_as='hard',
-            color_range=4)
-        expected_csv_path = 'tests/data/MT_OldBaldy_usgs_results.csv'
+    # def test_MT_OldBaldy(self):
+    #     """
+    #     Tests that usgsDemo produces the expected results.
+    #     Data used for this test is the point features from MT_OldBaldyMountain_265833_1989_24000_geo_mosaic, which is from the final evaluation dataset.
+    #     This test is commented out beacuse the test data is too large to be commited to the repository.
+    #     """
+    #     log = init_test_log('Test_usgsDemo/test_MT_OldBaldy')
+    #     # args = '-p tests/uncommited_data/pred_segmentations/MT_OldBaldyMountain_265833_1989_24000_geo_mosaic* -t tests/uncommited_data/true_segmentations -m tests/uncommited_data/map_images/ -l tests/uncommited_data/legends'
+    #     pred_seg_dir = 'tests/uncommited_data/pred_segmentations'
+    #     args = SimpleNamespace(
+    #         pred_segmentations=[os.path.join(pred_seg_dir,f) for f in os.listdir(pred_seg_dir) if 'MT_OldBaldyMountain' in f], 
+    #         true_segmentations='tests/uncommited_data/true_segmentations', 
+    #         map_images='tests/uncommited_data/map_images',
+    #         legends='tests/uncommited_data/legends',
+    #         min_valid_range=0.1,
+    #         difficult_weight=0.7,
+    #         set_false_as='hard',
+    #         color_range=4)
+    #     expected_csv_path = 'tests/data/MT_OldBaldy_usgs_results.csv'
 
-        self.run_test_usgsDemo(args, expected_csv_path, log)
-        log.info('Test passed successfully')
+    #     self.run_test_usgsDemo(args, expected_csv_path, log)
+    #     log.info('Test passed successfully')
 
-    def test_CA_Elsinore(self):
-        log = init_test_log('Test_usgsDemo/test_CA_Elsinore')
-        # args = '-p tests/uncommited_data/pred_segmentations/CA_Elsinore* -t tests/uncommited_data/true_segmentations -m tests/uncommited_data/map_images/ -l tests/uncommited_data/legends'
-        pred_seg_dir = 'tests/uncommited_data/pred_segmentations'
-        args = SimpleNamespace(
-            pred_segmentations=[os.path.join(pred_seg_dir,f) for f in os.listdir(pred_seg_dir) if 'CA_Elsinore' in f],
-            true_segmentations='tests/uncommited_data/true_segmentations', 
-            map_images='tests/uncommited_data/map_images',
-            legends='tests/uncommited_data/legends',
-            min_valid_range=0.1,
-            difficult_weight=0.7,
-            set_false_as='hard',
-            color_range=4)
-        expected_csv_path = 'tests/data/CA_Elsinore_usgs_results.csv'
+    # def test_CA_Elsinore(self):
+    #     """
+    #     Tests that usgsDemo produces the expected results.
+    #     Data used for this test is the point features from CA_Elsinore, which is from the validation dataset.
+    #     This test is commented out beacuse the test data is too large to be commited to the repository.
+    #     """
+    #     log = init_test_log('Test_usgsDemo/test_CA_Elsinore')
+    #     # args = '-p tests/uncommited_data/pred_segmentations/CA_Elsinore* -t tests/uncommited_data/true_segmentations -m tests/uncommited_data/map_images/ -l tests/uncommited_data/legends'
+    #     pred_seg_dir = 'tests/uncommited_data/pred_segmentations'
+    #     args = SimpleNamespace(
+    #         pred_segmentations=[os.path.join(pred_seg_dir,f) for f in os.listdir(pred_seg_dir) if 'CA_Elsinore' in f],
+    #         true_segmentations='tests/uncommited_data/true_segmentations', 
+    #         map_images='tests/uncommited_data/map_images',
+    #         legends='tests/uncommited_data/legends',
+    #         min_valid_range=0.1,
+    #         difficult_weight=0.7,
+    #         set_false_as='hard',
+    #         color_range=4)
+    #     expected_csv_path = 'tests/data/CA_Elsinore_usgs_results.csv'
 
-        self.run_test_usgsDemo(args, expected_csv_path, log)
-        log.info('Test passed successfully')
+    #     self.run_test_usgsDemo(args, expected_csv_path, log)
+    #     log.info('Test passed successfully')
