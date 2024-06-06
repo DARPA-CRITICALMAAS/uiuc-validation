8dd4c386f6199bb790185bed63aa29aea45d4e2c
Albert Bode <abode@illinois.edu>
2024-06-06 14:34:20 -0500

    Added validationDemo tests

--- tests/test_validationDemo.py
+++ tests/test_validationDemo.py
@@ -1,44 +1,43 @@
 import os
-import usgsDemo
 import pandas as pd
+import validationDemo
 from types import SimpleNamespace
 from tests.utilities import init_test_log
 
-class Test_usgsDemo:
-    def run_test_usgsDemo(self, args, expected_csv_path, log):
+class Test_validationDemo:
+    def run_test_validationDemo(self, args, expected_csv_path, log):
         """
-        Runs usgsDemo with the given arguments.
-        Tests that the csv of results produced by usgsDemo match the expected results.
-        This function is not a test itself.
+        Runs validationDemo with the given arguments.
+        Tests that the csv of results produced by validationDemo is the same as the expected csv.
+        This function is not a test itself
         """
-        log.info(f'Running usgsDemo with args: {args}')
+        log.info(f'Running validationDemo with args: {args}')
         os.makedirs('tests/tmp', exist_ok=True)
-        test_csv_path = 'tests/tmp/usgsDemo_results.csv'
+        test_csv_path = 'tests/tmp/validationDemo_results.csv'
         args.output = os.path.dirname(test_csv_path)
-        
-        # Run usgsDemo to get our test results
-        usgsDemo.main(args)
+
+        # Run validationDemo to get our test results
+        validationDemo.main(args)
         test_csv = pd.read_csv(test_csv_path).sort_values(by=['Map', 'Feature']).reset_index(drop=True)
-        test_csv = test_csv[['Map', 'Feature', 'F1 Score', 'Precision', 'Recall']]
-        
+        test_csv = test_csv[['Map', 'Feature', 'F1 Score', 'Precision', 'Recall', 'USGS F1 Score', 'USGS Precision', 'USGS Recall']]
+
         # Load the expected results
         expected_csv = pd.read_csv(expected_csv_path).sort_values(by=['Map', 'Feature']).reset_index(drop=True)
-        expected_csv = expected_csv[['Map', 'Feature', 'USGS F1 Score', 'USGS Precision', 'USGS Recall']]
-        expected_csv.rename(columns={'USGS F1 Score': 'F1 Score', 'USGS Precision': 'Precision', 'USGS Recall': 'Recall'}, inplace=True)
+        expected_csv = expected_csv[['Map', 'Feature', 'F1 Score', 'Precision', 'Recall', 'USGS F1 Score', 'USGS Precision', 'USGS Recall']]
 
-        # Check that the results are consistent.
+        # Check that the results are consistent
         log.info(f'Test CSV:\n{test_csv.to_string()}\nExpected CSV:\n{expected_csv.to_string()}')
         assert (test_csv).equals(expected_csv)
-        
+
         # Clean up
         os.remove(test_csv_path)
 
     def test_mock_map(self):
         """
-        Tests that usgsDemo produces the expected results.
-        Data used for this test is a mock map that is 100x100 pixels. The mock map has two point features, 2_pt and 3_pt and two poly features L_poly, TZ_poly.
+        Tests that validationDemo produces the expected results.
+        Data used for this test is the point features from mock_map, which is from the final evaluation dataset.
         """
-        log = init_test_log('Test_usgsDemo/test_mock_map')
+        log = init_test_log('Test_validationDemo/test_mock_map')
         # args = '-p tests/data/pred_segmentations -t tests/data/true_segmentations -m tests/data/map_images -l tests/data/legends --min_valid_range 5'
         pred_seg_dir = 'tests/data/pred_segmentations'
         args = SimpleNamespace(
@@ -49,54 +48,63 @@ class Test_usgsDemo:
             min_valid_range=5,
             difficult_weight=0.7,
             set_false_as='hard',
-            color_range=4)
+            color_range=4,
+            log='/dev/null',
+            verbose=False,
+            feedback=False)
         expected_csv_path = 'tests/data/mock_map_results.csv'
 
-        self.run_test_usgsDemo(args, expected_csv_path, log)
+        self.run_test_validationDemo(args, expected_csv_path, log)
         log.info('Test passed successfully')
 
     # def test_MT_OldBaldy_map(self):
     #     """
-    #     Tests that usgsDemo produces the expected results.
+    #     Tests that validationDemo produces the expected results.
     #     Data used for this test is the point features from MT_OldBaldyMountain_265833_1989_24000_geo_mosaic, which is from the final evaluation dataset.
     #     This test is commented out beacuse the test data is too large to be commited to the repository.
     #     """
-    #     log = init_test_log('Test_usgsDemo/test_MT_OldBaldy_map')
+    #     log = init_test_log('Test_validationDemo/test_MT_OldBaldy_map')
     #     # args = '-p tests/uncommited_data/pred_segmentations/MT_OldBaldyMountain_265833_1989_24000_geo_mosaic* -t tests/uncommited_data/true_segmentations -m tests/uncommited_data/map_images/ -l tests/uncommited_data/legends'
     #     pred_seg_dir = 'tests/uncommited_data/pred_segmentations'
     #     args = SimpleNamespace(
-    #         pred_segmentations=[os.path.join(pred_seg_dir,f) for f in os.listdir(pred_seg_dir) if 'MT_OldBaldyMountain' in f], 
+    #         pred_segmentations=[os.path.join(pred_seg_dir,f) for f in os.listdir(pred_seg_dir) if 'MT_OldBaldyMountain_265833_1989_24000_geo_mosaic' in f], 
     #         true_segmentations='tests/uncommited_data/true_segmentations', 
     #         map_images='tests/uncommited_data/map_images',
     #         legends='tests/uncommited_data/legends',
     #         min_valid_range=0.1,
     #         difficult_weight=0.7,
     #         set_false_as='hard',
-    #         color_range=4)
+    #         color_range=4,
+    #         log='/dev/null',
+    #         verbose=False,
+    #         feedback=False)
     #     expected_csv_path = 'tests/data/MT_OldBaldy_results.csv'
 
-    #     self.run_test_usgsDemo(args, expected_csv_path, log)
+    #     self.run_test_validationDemo(args, expected_csv_path, log)
     #     log.info('Test passed successfully')
 
     # def test_CA_Elsinore_map(self):
     #     """
-    #     Tests that usgsDemo produces the expected results.
+    #     Tests that validationDemo produces the expected results.
     #     Data used for this test is the point features from CA_Elsinore, which is from the validation dataset.
     #     This test is commented out beacuse the test data is too large to be commited to the repository.
     #     """
-    #     log = init_test_log('Test_usgsDemo/test_CA_Elsinore_map')
+    #     log = init_test_log('Test_validationDemo/test_CA_Elsinore_map')
     #     # args = '-p tests/uncommited_data/pred_segmentations/CA_Elsinore* -t tests/uncommited_data/true_segmentations -m tests/uncommited_data/map_images/ -l tests/uncommited_data/legends'
     #     pred_seg_dir = 'tests/uncommited_data/pred_segmentations'
     #     args = SimpleNamespace(
-    #         pred_segmentations=[os.path.join(pred_seg_dir,f) for f in os.listdir(pred_seg_dir) if 'CA_Elsinore' in f],
+    #         pred_segmentations=[os.path.join(pred_seg_dir,f) for f in os.listdir(pred_seg_dir) if 'CA_Elsinore' in f], 
     #         true_segmentations='tests/uncommited_data/true_segmentations', 
     #         map_images='tests/uncommited_data/map_images',
     #         legends='tests/uncommited_data/legends',
     #         min_valid_range=0.1,
     #         difficult_weight=0.7,
     #         set_false_as='hard',
-    #         color_range=4)
+    #         color_range=4,
+    #         log='/dev/null',
+    #         verbose=False,
+    #         feedback=False)
     #     expected_csv_path = 'tests/data/CA_Elsinore_results.csv'
 
-    #     self.run_test_usgsDemo(args, expected_csv_path, log)
+    #     self.run_test_validationDemo(args, expected_csv_path, log)
     #     log.info('Test passed successfully')
--- validationDemo.py
+++ validationDemo.py
@@ -9,7 +9,7 @@ from rich.progress import track
 import cmaas_utils.io as io
 from cmaas_utils.types import MapUnitType
 from cmaas_utils.logging import start_logger, changeConsoleHandler
-from src.grading import grade_poly_raster, usgs_grade_poly_raster, usgs_grade_pt_raster
+from src.grading import grade_point_raster, grade_poly_raster, usgs_grade_poly_raster
 from rich.logging import RichHandler
 
 LOGGER_NAME = 'DARPA_CMAAS_VALIDATION'
@@ -70,7 +70,7 @@ def parse_command_line():
     
     parser = argparse.ArgumentParser(description='', add_help=False)
     required_args = parser.add_argument_group('required arguments', '')
-    required_args.add_argument('-d', '--data', 
+    required_args.add_argument('-p', '--pred_segmentations', 
                         type=parse_data,
                         required=True,
                         nargs='+',
@@ -80,30 +80,47 @@ def parse_command_line():
                         type=parse_directory,
                         required=True,
                         help='Directory containing the true raster segmentations to score the data against.')
-    required_args.add_argument('-b','--base_maps',
+    required_args.add_argument('-m','--map_images',
                         type=parse_directory,
                         required=True,
                         help='Directory containing the base map images for the segmentations')
     required_args.add_argument('-l', '--legends',
                         type=parse_directory,
                         required=True,
-                        default=None,
-                        help='Optional directory containing precomputed legend data in USGS json format. If option is \
-                              provided, the pipeline will use the precomputed legend data instead of generating its own.')
+                        help='Directory containing precomputed legend data in USGS json format.')
 
     # Optional Arguments
     optional_args = parser.add_argument_group('optional arguments', '')
-    optional_args.add_argument('-f','--feature_type',
-                        type=parse_feature,
-                        default=MapUnitType.POLYGON,
-                        help='Type of features that will be graded on, will be used if the feature type can\'t be \
-                               detected from the file name. Available features are Point or Polygon') 
     optional_args.add_argument('-o', '--output',
                         default='results',
                         help='Directory to write the validation feedback to. Defaults to "results"')
+    # optional_args.add_argument('-f','--feature_type',
+    #                     type=parse_feature,
+    #                     default=MapUnitType.POLYGON,
+    #                     help='Type of features that will be graded on, will be used if the feature type can\'t be \
+    #                            detected from the file name. Available features are Point or Polygon') 
     optional_args.add_argument('--log',
                         default='logs/Latest.log',
                         help='Option to set the file logging will output to. Defaults to "logs/Latest.log"')
+    optional_args.add_argument('--min_valid_range',
+                        type=float,
+                        default=0.1,
+                        help='Maximum distance in % of the largest size of the image (diagonal) ' +
+                             'between a predicted pixel vs. a true one that will be considered ' +
+                             'as valid to include in the scoring. Defaults to 0.1')
+    optional_args.add_argument('--difficult_weight',
+                        type=float,
+                        default=0.7,
+                        help='Weight to give difficult points in the F1 score, range is a float within [0, 1]. Defaults to 0.7')
+    optional_args.add_argument('--set_false_as',
+                        default='hard',
+                        help='when set to "hard" the pixels that are not within the true polygon area will be considered hard.'
+                             'Set how to treat false positives and false negatives. Options are "hard" or "easy". Defaults to "hard"')
+    optional_args.add_argument('--color_range',
+                        type=int,
+                        default=4,
+                        help='The range of color variation to consider for the legend color. Defaults to 4')
+    
     # Flags
     flag_group = parser.add_argument_group('Flags', '')
     flag_group.add_argument('-h', '--help',
@@ -117,12 +134,10 @@ def parse_command_line():
                         help='Enable saving debugging feedback images.')
 
     args = parser.parse_args()
-    args.data = post_parse_data(args.data)
+    args.pred_segmentations = post_parse_data(args.pred_segmentations)
     return args
 
-def main():
-    args = parse_command_line()
-
+def main(args):
     # Start logger
     if args.verbose:
         global FILE_LOG_LEVEL, STREAM_LOG_LEVEL
@@ -134,10 +149,11 @@ def main():
     # Log info statement to console even if in warning only mode
     log.handlers[1].setLevel(logging.INFO)
     log.info(f'Running pipeline on {os.uname()[1]} with following parameters:\n' +
-            f'\tFeature type : {args.feature_type}\n' +
-            f'\tData         : {args.data}\n' +
+            # f'\tFeature type : {args.feature_type}\n' +
+            f'\tPred Data    : {args.pred_segmentations}\n' +
             f'\tTrue Data    : {args.true_segmentations}\n' +
-            f'\tMaps         : {args.base_maps}\n' +
+            f'\tMaps         : {args.map_images}\n' +
+            f'\tLegends      : {args.legends}\n' +
             f'\tOutput       : {args.output}\n')
     log.handlers[1].setLevel(STREAM_LOG_LEVEL)
 
@@ -146,21 +162,21 @@ def main():
         os.makedirs(args.output)
 
     results_df = pd.DataFrame(columns = [
-        'Map','Feature', 'F1 Score', 'Precision', 'Recall', 'IoU Score (polys)',
-        'USGS F1 Score (polys)', 'USGS Precision (polys)', 'USGS Recall (polys)', 
-        'Mean matched distance (pts)', 'Matched (pts)', 'Unmatched (pts)', 'Missing (pts)'])
+        'Map','Feature', 'F1 Score', 'Precision', 'Recall', 'IoU Score',
+        'USGS F1 Score', 'USGS Precision', 'USGS Recall', 
+        'Mean matched distance Points', 'Matched Points', 'Unmatched Points', 'Missing Points'])
 
-    log.info(f'Starting grading of {len(args.data)} files')
+    log.info(f'Starting grading of {len(args.pred_segmentations)} files')
     last_map_filepath = None
     last_legend_filepath = None
-    potential_map_names = [os.path.splitext(m)[0] for m in os.listdir(args.base_maps) if m.endswith('.tif')]
-    pbar = track(args.data)
+    potential_map_names = [os.path.splitext(m)[0] for m in os.listdir(args.map_images) if m.endswith('.tif')]
+    pbar = track(args.pred_segmentations)
     logging_handler = changeConsoleHandler(log, RichHandler(level=STREAM_LOG_LEVEL))
     for pred_filepath in pbar:
         feature_name = os.path.basename(os.path.splitext(pred_filepath)[0])
         feature_type = MapUnitType.from_str(feature_name.split('_')[-1])
-        if feature_type == MapUnitType.UNKNOWN:
-            feature_type = args.feature_type
+        # if feature_type == MapUnitType.UNKNOWN:
+        #     feature_type = args.feature_type
         log.info(f'Processing {feature_name}')
         
         # Load data
@@ -172,7 +188,7 @@ def main():
             continue
 
         map_name = [m for m in potential_map_names if m in pred_filepath][0]
-        map_filepath = os.path.join(args.base_maps, map_name + '.tif')
+        map_filepath = os.path.join(args.map_images, map_name + '.tif')
         if map_filepath != last_map_filepath:
             map_image, crs, transform = io.loadGeoTiff(map_filepath)
             last_map_filepath = map_filepath
@@ -180,7 +196,7 @@ def main():
         if legend_filepath != last_legend_filepath:
             map_legend = io.loadLegendJson(legend_filepath)
             last_legend_filepath = legend_filepath
-        feature = [f for f in map_legend.features if f.label in pred_filepath][0]
+        feature = [f for f in map_legend.features if f.label.replace(' ', '_') in pred_filepath][0]
 
         feedback_image = None
         if args.feedback:
@@ -188,30 +204,33 @@ def main():
             
 
         if feature_type == MapUnitType.POINT:
-            results, feedback_image = usgs_grade_pt_raster(pred_image, true_image, feedback_image=feedback_image)
+            results, feedback_image = grade_point_raster(pred_image, true_image, feedback_image=feedback_image, min_valid_range=args.min_valid_range)
             results['Map'] = map_name
             results['Feature'] = feature_name
+            results['USGS F1 Score'] = results['F1 Score']
+            results['USGS Precision'] = results['Precision']
+            results['USGS Recall'] = results['Recall']
 
             #log.info(f'Results for {feature_name} : {results}')
             log.info(f'Results for "{feature_name}" : ' + 
                 f'F1 Score : {results["F1 Score"]:.2f}, ' + 
                 f'Precision : {results["Precision"]:.2f}, ' +
                 f'Recall : {results["Recall"]:.2f}, ' +
-                f'Mean matched distance : {results["Mean matched distance"]:.2f}, ' +
-                f'Matched pts : {results["Matched pts"]:.2f}, ' +
-                f'Missing pts : {results["Missing pts"]:.2f}, ' +
-                f'Unmatched pts : {results["Unmatched pts"]:.2f}'
+                f'Mean matched distance : {results["Mean Matched Distance"]:.2f}, ' +
+                f'Matched pts : {results["Matched Points"]:.2f}, ' +
+                f'Missing pts : {results["Missing Points"]:.2f}, ' +
+                f'Unmatched pts : {results["Unmatched Points"]:.2f}'
             )
             results_df.loc[len(results_df)] = results
             
         if feature_type == MapUnitType.POLYGON:
             results, feedback_image = grade_poly_raster(pred_image, true_image, feedback_image=feedback_image)
-            USGS_results, _ = usgs_grade_poly_raster(pred_image, true_image, map_image, feature.bounding_box, feedback_image=feedback_image, difficult_weight=0.7)
+            USGS_results, _ = usgs_grade_poly_raster(pred_image, true_image, map_image, feature.bounding_box, feedback_image=feedback_image, difficult_weight=args.difficult_weight, color_range=args.color_range, set_false_as=args.set_false_as)
             results['Map'] = map_name
             results['Feature'] = feature_name
-            results['USGS F1 Score (polys)'] = USGS_results['F1 Score']
-            results['USGS Precision (polys)'] = USGS_results['Precision']
-            results['USGS Recall (polys)'] = USGS_results['Recall']
+            results['USGS F1 Score'] = USGS_results['F1 Score']
+            results['USGS Precision'] = USGS_results['Precision']
+            results['USGS Recall'] = USGS_results['Recall']
 
             # log.info(f'Results for {feature_name} {results}')
             log.info(f'Results for "{feature_name}" : ' + 
@@ -219,9 +238,9 @@ def main():
                 f'Precision : {results["Precision"]:.2f}, ' +
                 f'Recall : {results["Recall"]:.2f}, ' +
                 f'IoU Score : {results["IoU Score"]:.2f}, ' +
-                f'USGS F1 Score: {results["USGS F1 Score (polys)"]:.2f}, ' +
-                f'USGS Precision : {results["USGS Precision (polys)"]:.2f}, ' +
-                f'USGS Recall : {results["USGS Recall (polys)"]:.2f}'
+                f'USGS F1 Score: {results["USGS F1 Score"]:.2f}, ' +
+                f'USGS Precision : {results["USGS Precision"]:.2f}, ' +
+                f'USGS Recall : {results["USGS Recall"]:.2f}'
             )    
             results_df.loc[len(results_df)] = results
 
@@ -230,9 +249,10 @@ def main():
             io.saveGeoTiff(os.path.join(args.output, 'val_' + feature_name + '.tif'), feedback_image, crs, transform)
     changeConsoleHandler(log, logging_handler)
 
-    csv_path = os.path.join(args.output, '#dataset_results.csv')
+    csv_path = os.path.join(args.output, 'validationDemo_results.csv')
     log.info(f'Finished grading, saving results to {csv_path}')    
     results_df.to_csv(csv_path)
 
 if __name__=='__main__':
-    main()
+    args = parse_command_line()
+    main(args)
